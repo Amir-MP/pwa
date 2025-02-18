7de059cb515065c131e7125a9f0d2a55f87e9039
Amir MP <a.manafpour@daya.com>
2025-02-18 11:46:02 +0330

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,6 +52,18 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="/qr"
+        >
+          QR Code
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="/finger-print"
+        >
+          Finger Print
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
