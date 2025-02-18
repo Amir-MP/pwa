5af66830a6762b7c6f8a5bbaae9aa9864aec5306
Amir MP <a.manafpour@daya.com>
2025-02-18 12:41:06 +0330

    .

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,26 @@ const nextConfig: NextConfig = {
   },
 };
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        }
+      }
+    }
+  ]
 });
+
 module.exports = withPWA({});
 
 export default withPWA(nextConfig);
